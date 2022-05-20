854d7c4ef65d6abac07f7d0a9fa3fe36ac3e84d5
changwoolim <limcw@zarathu.com>
2022-05-20 17:48:24 +0900

    Update app.R

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(echarts4r)
 library(dplyr)
@@ -14,46 +5,85 @@ library(stringi)
 
 data <- mtcars %>% as.data.frame()
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-  titlePanel("Echars4r - Scatter Plot w/ mtcars"),
+  titlePanel("plotGen - Plot Genesis"),
   sidebarLayout(
     sidebarPanel(
       selectizeInput(
-        inputId = "criteria", 
+        inputId = "criteria",
         label = "criteria",
-        choices = mtcars %>% colnames(), 
+        choices = mtcars %>% colnames(),
         selected = NULL
       ),
+      checkboxGroupInput(
+        inputId = "options",
+        label = "Option for Plot",
+        choices = c("Show X-Axis" = "xaxis", "Show Y-Axis" = "yaxis"),
+        selected = c("xaxis", "yaxis"),
+      ),
       selectizeInput(
-        inputId = "describe", 
-        label = "describe",
-        choices = mtcars %>% colnames(), 
+        inputId = "describe",
+        label = "Select Variable to Describe (Maximum: 2)",
+        choices = mtcars %>% colnames(),
         selected = NULL,
-        multiple = TRUE 
+        multiple = TRUE,
+        options = list(maxItems = 2),
         # 를 넣으면 여러개가 다 그래프에 나와야 하는데, input$describe로는 오류가 남.
         # input$describe의 type은 character인데, 이걸 list나 vector로 변환해도 한번에 처리 는 어려운 것 같음.
         # 그런데 mode()로 볼때는 character였는데, input$describe[1] 이런식의 호출도 되는 것으로 봐서 또 다른것인지 의문이 듦.
       ),
     ),
     mainPanel(
+      textOutput("test"),
       # 처음에 plotOutput으로 충분한지 알았으나, reference 찾고 난 후 eharts4r에 맞는 output이 있다는 것을 알았음.
       # 패키지별로 output이 따로 존재한다고 생각해야 할 것 같음.
       echarts4rOutput("scatterPlot"),
-      textOutput("test"),
     )
   )
 )
 
 server <- function(input, output) {
   output$scatterPlot <- renderEcharts4r({
-    # e_charts(input$criteria)로는 오류 발생. e_charts_로만 가능한데 이 이유를 찾아보아야 함.
-    data %>%
-      e_charts_(input$criteria) %>%
-      e_scatter_(input$describe)
+    
+    axis <- function(e, axis){
+     # if(axis %in% input$options){
+     #    return(
+     #      e %>% 
+     #      e_x_axis(index=0, show=FALSE, axisLabel=axis)
+     #    )
+     #  } else {
+     #    return(e)
+     #  }
+      return(
+        e %>% 
+          e_x_axis(index=0, show=(FALSE || (axis %in% input$options)), axisLabel=axis)
+      )
+    }
+    
+    plot <- function(){
+      return(
+        data %>%
+        e_charts_(input$criteria) %>%
+        e_scatter_(input$describe) %>% 
+        e_toolbox_feature (
+          feature = c("saveAsImage")
+        ) %>% 
+        axis("xaxis") %>% 
+        axis("yaxis")
+      )
+    }
+    
+    if(!is.null(input$describe)) { # Describe에 아무것도 입력되지 않은 경우.
+      plot()
+      # e_charts(input$criteria)로는 오류 발생. e_charts_로만 가능한데 이 이유를 찾아보아야 함.
+      # e_charts 랑 e_charts_ 는 파라미터를 names로 보느냐 character로 받느냐의 차이.
+    } 
+      
   })
-
-  output$test <- renderText(stri_split_fixed(input$describe, " ", omit_empty = TRUE))
+  
+  output$test <- renderText({
+    (function (x) substr(x,1,1)) (as.list(input$describe))
+  })
 }
 
 shinyApp(ui = ui, server = server)
