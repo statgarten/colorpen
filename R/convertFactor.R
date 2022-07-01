6705307d1dcda25534f51989f8df8017ca436597
changwoolim <limcw@zarathu.com>
2022-07-01 09:10:10 +0900

    Add params

--- R/convertFactor.R
+++ R/convertFactor.R
@@ -1,7 +1,4 @@
 #' @export
-
-source("R/detectFactor.R")
-
 convertFactor <- function(data, order = NULL) {
   if(is.null(order)) order <- detectFactor(data)
   for(col in 1:(length(order))) if(order[col] == "Factor") data[, col] <- as.factor(data[, col])
--- R/plotGen.R
+++ R/plotGen.R
@@ -4,12 +4,19 @@
 #' @import tidyverse
 #' @import plotly
 #'
+#'@param data Data to describe in graph
+#'@param type Type of graph
+#'@param criteria X-axis variable in data
+#'@param describe Y-axis variable in data
+#'@param options Additional Options
 
-plotGen <- function(data, criteria, description = NULL){
+plotGen <- function(data, type, criteria, describe = NULL, options){
+  data <- data %>% as.data.frame()
+
   f_aes <- function(x, y){
     colour <- NULL
-    if(criteria != 'NA') {
-      colour <- criteria
+    if(x != 'NA') {
+      colour <- x
     }
     # if("single_variable" %in% input$dev){
     #   return(
@@ -25,9 +32,14 @@ plotGen <- function(data, criteria, description = NULL){
     )
   }
 
-  g <-data %>%
-        ggplot(f_aes(x = criteria, y = description)) +
-        geom_point()
+  f_geom <- function(){
+
+  }
+
+  g <- data %>%
+    ggplot(f_aes(x = criteria, y = describe)) +
+    geom_point()
+
 
   return(g)
 }
