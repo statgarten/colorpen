81afde15b5f77544c60423191eeb676bd61a6c39
changwoolim <limcw@zarathu.com>
2022-07-04 10:01:11 +0900

    Add graph

--- R/bivariate.R
+++ R/bivariate.R
@@ -1,15 +1,55 @@
 #' @export
 
 bivariate <- function(type, vartypeX, vartypeY) {
-  switch(type,
-    "scatter" = {
-      return(geom_point())
-    },
-    "line" = {
-      return(geom_line())
-    },
-    "jitter" = {
-      return(geom_jitter())
-    },
-  )
+  if (vartypeX == "Factor" & vartypeY == "Factor") {
+    switch(type,
+      "count" = {
+        return(geom_count())
+      },
+      "jitter" = {
+        return(geom_jitter())
+      },
+    )
+  } else if (vartypeX == "Factor" & vartypeY == "Continuous") {
+    switch(type,
+      "col" = {
+        return(geom_col())
+      },
+      "box" = {
+        return(geom_boxplot())
+      },
+      "dot" = {
+        return(geom_dotplot())
+      },
+      "violin" = {
+        return(geom_violin())
+      },
+    )
+  } else if (vartypeX == "Continuous" & vartypeY == "Factor") {
+
+  } else {
+    switch(type,
+      "scatter" = {
+        return(geom_point())
+      },
+      "line" = {
+        return(geom_line())
+      },
+      "quantile" = {
+        return(geom_quantile())
+      },
+      "lm" = {
+        return(geom_smooth(method = lm))
+      },
+      "area" = {
+        return(geom_area())
+      },
+      "line" = {
+        return(geom_line())
+      },
+      "step" = {
+        return(geom_step(direction = "hv")) # direction of stairs: 'vh' for vertical then horizontal, 'hv' for horizontal then vertical, or 'mid' for step half-way between adjacent x-values.
+      }
+    )
+  }
 }
--- R/univariate.R
+++ R/univariate.R
@@ -1,21 +1,29 @@
 #' @export
 
 univariate <- function(type, vartypeX) {
-  switch(type,
-    "histogram" = {
-      return(geom_histogram()) # binwidth 넣을 필요 있을수도
-    },
-    "area" = {
-      return(geom_area(stat = "bin"))
-    },
-    "dot" = {
-      return(geom_dotplot())
-    },
-    "freqpoly" = {
-      return(geom_freqpoly())
-    },
-    "density" = {
-      return(geom_density())
-    }
-  )
+  if (vartypeX == "Factor") {
+    switch(type,
+      "bar" = {
+        return(geom_bar())
+      },
+    )
+  } else if (vartypeX == "Continuous") {
+    switch(type,
+      "histogram" = {
+        return(geom_histogram()) # binwidth 넣을 필요 있을수도
+      },
+      "area" = {
+        return(geom_area(stat = "bin"))
+      },
+      "dot" = {
+        return(geom_dotplot())
+      },
+      "freqpoly" = {
+        return(geom_freqpoly())
+      },
+      "density" = {
+        return(geom_density())
+      }
+    )
+  }
 }
